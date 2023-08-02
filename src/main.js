2487d2e3c2cf152bbd61083e7a27f415b5bfafcc
Jeffrey Segovia <jsegovia@yondu.com>
2023-08-02 16:26:13 +0800

    init commit

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import { createApp } from 'vue'
+
+import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
+
+app.use(pinia)
+app.use(vuetify)
+app.use(router)
+
+app.mount('#app')
